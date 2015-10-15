1485d2697e1683f7fe64feef3e25a59976b00bf3
liuzhouhua <liuzhouhua@akey.me>
2015-10-15 18:58:01 +0800

    15/10/15 02

--- src/com/example/mymusic/LocalSingerFragment.java
+++ src/com/example/mymusic/LocalSingerFragment.java
@@ -1,5 +1,8 @@
 package com.example.mymusic;
 
+import com.example.mymusic.view.SideBar;
+import com.example.mymusic.view.SideBar.OnTouchingLetterChangedListener;
+
 import android.annotation.SuppressLint;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
@@ -7,22 +10,38 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.View.OnClickListener;
+import android.widget.ListView;
+import android.widget.TextView;
 
 @SuppressLint("NewApi")
-public class LocalSingerFragment extends Fragment implements OnClickListener{
+public class LocalSingerFragment extends Fragment{
+	
+	private ListView mSingerList;
+	private TextView mLetterText;
+	private SideBar mSideBar;
 
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		return inflater.inflate(R.layout.local_singer_fragment, null);
 	}
 	
+	
 	@Override
-	public void onClick(View arg0) {
-		// TODO Auto-generated method stub
-		
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		mSingerList = (ListView) getActivity().findViewById(R.id.local_singer_list);
+		mLetterText = (TextView) getActivity().findViewById(R.id.local_singer_letter);
+		mSideBar = (SideBar) getActivity().findViewById(R.id.local_singer_sidebar);
+		mSideBar.setTextView(mLetterText);
+		mSideBar.setOnTouchingLetterChangedListener(new OnTouchingLetterChangedListener() {
+			
+			@Override
+			public void onTouchingLetterChanged(String s) {
+				
+			}
+		});
 	}
 
 }
--- src/com/example/mymusic/view/SideBar.java
+++ src/com/example/mymusic/view/SideBar.java
@@ -6,22 +6,29 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Typeface;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.TextView;
 
 public class SideBar extends View {
+	
+	private static final String TAG = "SideBar";
 
 	public static String letter[]  = {"A", "B", "C", "D", "E", "F", "G", "H", "I",
 		"J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
 		"W", "X", "Y", "Z", "#"};
 	
+	//画字母的画笔
 	private Paint paint = new Paint();
+	//画选中时的矩形框
 	private Paint rectPaint = new Paint();
-	private int rectwidth = 0;
-	private boolean showBg = false;
+	private float rectwidth = 0;
 	private int choose = -1;
 	private int scrollChoose = -1;
 	private OnTouchingLetterChangedListener onTouchingLetterChangedListener;
+	//屏幕中间显示的大的选中的字母
+	private TextView textView;
 	
 	public SideBar(Context context) {
 		super(context);
@@ -40,32 +47,30 @@ public class SideBar extends View {
 	
 	private void init(){
 		rectPaint.setColor(Color.parseColor("#CCCCCC"));
-		rectwidth = (int) paint.measureText("#");
+		rectwidth = paint.measureText("#");
 	}
 	
 	@Override
 	protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
-		if(showBg){
-			canvas.drawColor(Color.parseColor("#CCCCCC"));
-		}
 		int height = getHeight();
 		int width = getWidth();
 		int singleHeight = height/letter.length;
-		int rectX = (int) (width/2-paint.measureText(letter[i])/2-rectwidth);
-		int rectX2 = rectX + 3*rectwidth;
+		float rectX = width/2-paint.measureText("#")/2-rectwidth;
+		float rectX2 = rectX + 3*rectwidth;
 		
 		for(int i=0;i<letter.length;i++){
 			paint.setColor(Color.parseColor("#c0c0c0"));
 			paint.setAntiAlias(true);
 			paint.setTypeface(Typeface.DEFAULT_BOLD);
 			paint.setTextSize(20);
+			float x = width/2-paint.measureText(letter[i])/2;
+			float y = singleHeight*i+singleHeight;
 			if (i == choose) {
 				paint.setColor(Color.parseColor("#ff2020"));
 				paint.setFakeBoldText(true);
+				canvas.drawRect(rectX,y-singleHeight/2 , rectX2, y+rectwidth, rectPaint);
 			}
-			float x = width/2-paint.measureText(letter[i])/2;
-			float y = singleHeight*i+singleHeight;
 			canvas.drawText(letter[i], x, y, paint);
 			paint.reset();
 		}
@@ -78,13 +83,25 @@ public class SideBar extends View {
 		final OnTouchingLetterChangedListener listener = onTouchingLetterChangedListener;
 		final int oldChoose = choose;
 		final int c = (int) (y/getHeight()*letter.length);
-		
 		switch(action){
-		case MotionEvent.ACTION_UP:
-			if(oldChoose!=c){
-				choose = c;
+		case MotionEvent.ACTION_DOWN:
+			if(oldChoose!=c && listener!=null){
+				textView.setVisibility(View.VISIBLE);
+				textView.setText(letter[c]);
+				doOnActionDown(c);
 			}
 			
+			break;
+		case MotionEvent.ACTION_MOVE:
+			if(oldChoose!=c && listener!=null){
+				textView.setVisibility(View.VISIBLE);
+				textView.setText(letter[c]);
+				doOnActionDown(c);
+			}
+			break;
+		case MotionEvent.ACTION_UP:
+			textView.setVisibility(View.GONE);
+			invalidate();
 			break;
 		default:
 			
@@ -92,7 +109,7 @@ public class SideBar extends View {
 		}
 		
 		
-		return super.dispatchTouchEvent(event);
+		return true;
 	}
 	
 	
@@ -105,5 +122,31 @@ public class SideBar extends View {
 			OnTouchingLetterChangedListener onTouchingLetterChangedListener) {
 		this.onTouchingLetterChangedListener = onTouchingLetterChangedListener;
 	}
+	
+	/* 
+    * 当侧边栏被按下的动作 
+    * @param c 
+    */  
+   private void doOnActionDown(int c) {  
+       if (c > 0 && c < letter.length) {
+    	   onTouchingLetterChangedListener.onTouchingLetterChanged(letter[c]);
+               choose = c;  
+               invalidate();
+       } 
+   }
+
+   public void setTextView(TextView textView) {
+	   this.textView = textView;
+   }
+
+
+   //设置listview滚动时候调用它 
+   public void setColorWhenListViewScrolling(int c){
+	   if (scrollChoose != c) {
+		   scrollChoose = c;
+           choose = c;  
+           invalidate();  
+       } 
+   }
 
 }
